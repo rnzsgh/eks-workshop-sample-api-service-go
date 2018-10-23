497083d96d70145e137a688c1b30f0ddf864f1fa
Garudan4321 <44412839+Garudan4321@users.noreply.github.com>
2018-10-23 15:06:04 -0400

    Update main.go
    Updated message to hello super world

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - now"}
+		res := &response{Message: "Hello smart World - now"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
