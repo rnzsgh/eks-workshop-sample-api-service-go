777c62836090a39455a36ea0484ef2905ed5ac97
eliran-allcloud <41681676+eliran-allcloud@users.noreply.github.com>
2019-01-05 17:44:28 +0200

    update
    true

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World v2"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
