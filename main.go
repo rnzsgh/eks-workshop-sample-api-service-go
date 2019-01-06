d7f7843591167adcd80f9548c9b38d81708b0513
eliran-allcloud <41681676+eliran-allcloud@users.noreply.github.com>
2019-01-06 15:35:04 +0200

    PR

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World directly to master"}
+		res := &response{Message: "Hello World - PR first"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
