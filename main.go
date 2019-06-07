fc7b43ec6a17d10a8d64c737c5efbdd28216ec4b
okostiukromexsoft <49395201+okostiukromexsoft@users.noreply.github.com>
2019-06-07 09:52:44 +0300

    Change some text in message

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Test text"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
