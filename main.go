9188b78b6969300689e803ca9a2182d7caa2f222
rohan <rohannayakwadi@gmail.com>
2019-07-31 11:02:15 -0500

    Update main.go

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello world -this is rohan's first EKS build- the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
