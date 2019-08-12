10d9df335824b702e921c8ce61b40b6e09ec9224
Nikoloz Bokeria <51808034+nikolozz@users.noreply.github.com>
2019-08-12 16:13:13 +0400

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello From VOBI!!!"}
+		res := &response{Message: "Hello"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
