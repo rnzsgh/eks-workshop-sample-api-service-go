f78d5497dd08a473806a6c20e8982deb8ded833d
rameshsri89 <ramesh.srinivasan@precisionit.co.in>
2019-08-19 16:18:24 +0530

    Change the message text

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Commit Changes"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Commit Changes")
 	})
 	http.ListenAndServe(":8080", nil)
 }
