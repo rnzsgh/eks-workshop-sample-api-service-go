e36d5d00e18b86ad8a44668fe16ca6bbb174cb5f
hirfujit <hirfujit@yahoo-corp.jp>
2020-03-08 00:30:03 +0900

    change message

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World by Hiroki2-3!"}
+		res := &response{Message: "Hello World by Hiroki3-1!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
