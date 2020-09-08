0e1a9f55a0bac5797ea1d7c1149e2d2136938b7f
tannydobreva <46463124+tannydobreva@users.noreply.github.com>
2020-09-07 22:55:41 -0700

    Update

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World New"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
