50f78fa2bc7a695a9948726f7cc33745073de12d
Logictek <Logictek@users.noreply.github.com>
2021-04-12 18:02:45 -0400

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
