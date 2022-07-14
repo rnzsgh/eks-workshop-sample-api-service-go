76f71cf35617c95c1fc859b724f6660563daeed0
Logictek <Logictek@users.noreply.github.com>
2022-07-14 13:53:32 -0400

    new commit
    updated feature push to eks

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Update to Feature v1.1"}
+		res := &response{Message: "Update to Feature v1.2"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
