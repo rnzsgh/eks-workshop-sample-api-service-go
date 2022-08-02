8cab1546d8c5cd542718a0036462a9d96fa2f2b5
feyzxf <73056587+feyzxf@users.noreply.github.com>
2022-08-02 12:04:38 +0800

    Update main.go to test change

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Test CI/CD"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
