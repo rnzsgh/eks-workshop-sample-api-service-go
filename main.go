2d24bf97d5d15780cf07ef7c9ba9316afcb43c4a
Adil Can <adil_can@hotmail.com>
2023-10-19 13:14:14 -0700

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World v2"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
